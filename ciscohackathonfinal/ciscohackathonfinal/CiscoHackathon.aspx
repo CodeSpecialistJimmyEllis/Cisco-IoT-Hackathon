aa87e93ead5e8a96d69e08864caf64863a20e1b6
JimmyEllis <jimmy.ellisx@gmail.com>
2018-12-03 23:03:00 -0600

    cisco project addition
    Yo this is a project I created right in front of cisco.

---
+++ ciscohackathonfinal/ciscohackathonfinal/CiscoHackathon.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="CiscoHackathon.aspx.cs" Inherits="ciscohackathonfinal.CiscoHackathon" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <style>
+
+        body { color: white;
+               font-size: 28px;
+               font-weight: bold;
+               background-color: black;
+               border: 1px solid black; 
+               border-radius: 8px;
+        }
+    </style>
+  <br />  <asp:Label ID="Label1" runat="server" Text="Label">Why are you there?</asp:Label>
+    <br />     <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+
+
+<br /><asp:Label ID="Label2" runat="server" Text="Label">Who do you want to meet there?</asp:Label>
+<br />     <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+    <br />
+        <br /><asp:Label ID="Label3" runat="server" Text="Label">What do you want to say about your experience there?</asp:Label>
+        <br /><asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+    <br />
+
+
+    <br /><asp:Label ID="Label4" runat="server" Text="ABSOLUTE AWARENESS"></asp:Label>
+    <br />
+    <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Button" />
+   <br /><asp:Image ID="Image1" runat="server" />
+    <br />
+    <br />
+    <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Submit" />
+
+    <img src="images/insertthispicture.png" style="height: 641px; width: 362px" /> 
+   <asp:Image ID="Image2" runat="server" ImageUrl="~/images/photo.JPG" Height="2039px" Width="2803px" /> 
+</asp:Content>
---
+++ ciscohackathonfinal/ciscohackathonfinal/CiscoHackathon.aspx.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+//using System.Net.Http;
+//using System.Net.Http.Headers;
+using System.Threading.Tasks;
+// using RestSharp;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading;
+
+namespace ciscohackathonfinal
+{
+    public partial class CiscoHackathon : System.Web.UI.Page
+    {
+
+        HttpWebRequest request; 
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            /*
+            // Create the web request  
+            request
+                = WebRequest.Create("https://10.10.20.11/api/contextaware/v1/maps/info/1871/mainBuilding/mainFloor") as HttpWebRequest;
+
+            // Add authentication to request  
+            request.Credentials = new NetworkCredential("learning", "learning");
+
+            request.Method = "GET";
+
+            //
+            System.Net.WebClient client = new System.Net.WebClient();
+            ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
+
+            //
+            // Get response  
+            using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+            {
+                // Get the response stream  
+                StreamReader reader = new StreamReader(response.GetResponseStream());
+
+                // Console application output  
+                //Console.WriteLine(reader.ReadToEnd());
+
+                Label1.Text = reader.ReadToEnd();
+            }
+
+       
+
+           // Thread.Sleep(5000); */
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+            Label4.Text = "WHY?" + TextBox1.Text.ToString() + "WHO" + TextBox2.Text.ToString() + "WHAT" + TextBox3.Text.ToString(); 
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            Label4.Text = "WHY?" + TextBox1.Text.ToString() + "WHO" + TextBox2.Text.ToString() + "WHAT" + TextBox3.Text.ToString(); 
+        }
+    }
+}
